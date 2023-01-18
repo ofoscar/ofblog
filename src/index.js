4a6cfc0d7f77dc2e2bef4cd6c0926317bcdb74fc
ofos <ktaoscar@gmail.com>
2023-01-17 21:54:52 -0700

    React router agregado

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './error-page';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Contact from './routes/contact';
+import { Posts } from './routes/index';
+import Root from './routes/root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Posts /> },
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
