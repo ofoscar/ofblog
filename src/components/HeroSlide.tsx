b277bd4250c45fe334260b465de550e5b04e46aa
ofoscar <ktaoscar@gmail.com>
2025-08-08 11:17:16 -0700

    refactor: enhance HeroSlide layout and styling for improved responsiveness and visual consistency

--- src/components/HeroSlide.tsx
+++ src/components/HeroSlide.tsx
@@ -17,14 +17,17 @@ function HeroSlide({
   onPrimaryClick,
 }: HeroSlideProps) {
   return (
-    <div className='w-full relative flex items-center justify-center overflow-hidden h-[480px] px-4 py-8 sm:px-8 md:px-16 lg:px-20 xl:px-[80px]'>
+    <div className='w-full relative flex items-end justify-center overflow-hidden h-[480px] px-4 py-8 sm:px-8 md:px-16 lg:px-20 xl:px-[80px]'>
       {/* Blurred Background Image */}
       <div
         className='absolute inset-0 sm:bg-cover bg-center'
         style={{
           backgroundImage: `url("${backgroundImage}")`,
           backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover', // Ensures the image covers the entire area
           filter: 'blur(4px)',
+          margin: '-10px', // Extends the image slightly beyond the edges
+          backgroundColor: '#000', // Adds a matching background color to blend
         }}
         aria-hidden='true'
       ></div>
@@ -36,28 +39,30 @@ function HeroSlide({
       ></div>
 
       {/* Content */}
-      <div className='relative z-10 text-left w-full max-w-[1280px]'>
-        <div className='flex-col max-w-[800px]'>
-          <h1
-            className='text-3xl sm:text-5xl md:text-[48px] font-bold mb-6 text-white w-full leading-none'
-            style={{
-              fontFamily: theme.typography.fontFamily.sans.join(', '),
-              textShadow: 'none',
-            }}
-          >
-            {title}
-          </h1>
-          <p
-            className='text-lg sm:text-xl md:text-[28px] text-[#EBE2D1] mb-8'
-            style={{
-              lineHeight: '1.2',
-              fontFamily: theme.typography.fontFamily.sans.join(', '),
-              textShadow: 'none',
-            }}
-          >
-            {subtitle}
-          </p>
-          <div className='flex flex-col items-start sm:flex-row gap-4 justify-center sm:items-center sm:justify-start'>
+      <div className='relative z-10 text-left w-full h-full max-w-[1280px] flex items-center sm:items-end sm:pb-4'>
+        <div className='flex flex-col sm:flex-row items-center sm:items-end w-full sm:justify-between gap-4 '>
+          <div className='flex flex-col gap-2 max-w-[700px]'>
+            <h1
+              className='text-3xl sm:text-5xl text-center sm:text-start font-bold text-white w-full leading-none'
+              style={{
+                fontFamily: theme.typography.fontFamily.sans.join(', '),
+                textShadow: 'none',
+              }}
+            >
+              {title}
+            </h1>
+            <p
+              className='text-lg sm:text-xl md:text-3xl  text-[#EBE2D1] text-center sm:text-start'
+              style={{
+                lineHeight: '1.2',
+                fontFamily: theme.typography.fontFamily.sans.join(', '),
+                textShadow: 'none',
+              }}
+            >
+              {subtitle}
+            </p>
+          </div>
+          <div className='flex-shrink-0'>
             <GlassButton
               className='gradient-border-wrapper text-lg md:text-2xl sm:w-[220px]'
               onClick={onPrimaryClick}
